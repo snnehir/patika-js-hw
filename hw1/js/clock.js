da9129fd2fad1a4889009b988f952ac4b39cb606
mint <snnehir21@gmail.com>
2022-02-08 01:11:51 +0300

    Ödev 1 Javascript Saat ve Karşılama

---
+++ hw1/js/clock.js
@@ -0,0 +1,13 @@
+const username = prompt("Adın nedir?")
+document.querySelector("#myName").innerHTML = username.trim().length > 0 ? username : 'İsmini vermek istemeyen kullanıcı' 
+
+
+document.querySelector("#myClock").onload.showTime = () =>{
+   
+   const weekday = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
+   let date_format = `${new Date().toLocaleTimeString()} ${weekday[new Date().getDay()]}`
+   document.querySelector("#myClock").innerHTML = date_format
+} 
+
+// refresh date
+setInterval(document.querySelector("#myClock").onload.showTime);
