070cc050cf2a9b7bd25210d11ec6ff700c5f9016
mint <snnehir21@gmail.com>
2022-02-08 01:03:04 +0300

    Yazım yanlışı düzeltildi :d

--- hw1/js/clock.js
+++ hw1/js/clock.js
@@ -4,7 +4,7 @@ document.querySelector("#myName").innerHTML = username.trim().length > 0 ? usern
 
 document.querySelector("#myClock").onload.showTime = () =>{
    
-   const weekday = ["Pazar","Pazartesi","Salo","Çarşamba","Perşembe","Cuma","Cumartesi"];
+   const weekday = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];
    let date_format = `${new Date().toLocaleTimeString()} ${weekday[new Date().getDay()]}`
    document.querySelector("#myClock").innerHTML = date_format
 } 
